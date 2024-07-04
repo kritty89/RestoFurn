8a98e81ba615df3e2e8725d924853c30e62a62e3
Adithya06-ca <adithyagorantla702@gmail.com>
2024-07-04 22:07:19 +0530

    Changes in About Page
    Added About.css and Content

--- Frontend/src/screens/About.js
+++ Frontend/src/screens/About.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import '../css/About.css';
 
 const About = () => {
   return (
-    <div><h2>About US</h2>
+    <div><h2 className='about_header'>About US</h2>
     <p>RestoFurn is a social enterprise retailer that accepts and resells donations of new and used furniture. All profits from their operations fund Habitat for Humanity, which builds homes
           for families in need of safe, decent, and affordable housing. Shopping at a Habitat ReStore supports local homebuilding 
           projects and helps divert items from landfills.</p>
@@ -15,6 +16,25 @@ const About = () => {
             chemicals like VOCs, allowing you to keep the health of your indoor environment in check. Because furniture is a long-term 
             investment, it’s even more important to consider sustainable furniture pieces that are ethically and responsibly made, as they can 
             make a significant difference for our homes, our families, and the planet.</p>
+
+        <section>
+        <h2>Company Policy</h2>
+        <p>
+          At our company, we strive to maintain the highest standards of ethics and responsibility in everything we do. Our company policy focuses on customer satisfaction, quality assurance, and continuous improvement.
+        </p>
+      </section>
+      <section>
+        <h2>Product Return Policy</h2>
+        <p>
+          If you are not completely satisfied with your purchase, you may return the product within 30 days of the purchase date. The product must be in its original condition and packaging to qualify for a return. Please contact our customer service team to initiate the return process.
+        </p>
+      </section>
+      <section>
+        <h2>Product Damage Policy</h2>
+        <p>
+          In the unlikely event that your product arrives damaged, please notify us within 7 days of receipt. We will provide instructions on how to return the damaged item and ensure you receive a refund promptly.
+        </p>
+      </section>
     </div>
   )
 }
